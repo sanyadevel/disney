a64430d9df4c3ec009847676891078fea31b356f
Alex Besleaga <sanyadevel@gmail.com>
2022-12-07 12:02:06 +0200

    some message

---
+++ src/components/Detail.js
@@ -0,0 +1,171 @@
+import React,{ useEffect,useState } from 'react';
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import db from "../firebase"
+function Detail() {
+    const { id } = useParams();
+    const [movie,setMovie] = useState();
+    //
+    // useEffect(()=>{
+    //     //Grab the movie info from DB
+    //     db.collection("movies")
+    //         .doc(id)
+    //         .get()
+    //         .then(()=>{
+    //             if(doc.exist){
+    //                 //save the movie data
+    //                 setMovie(doc.data());
+    //             }else{
+    //                 //redirect to home page
+    //
+    //             }
+    //         })
+    //
+    // },[])
+    // console.log(movie)
+    return (
+        <Container>
+            <Background>
+                <img
+                    src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/9C2152F120E2AAB1D65E394A9AA74E3F6A717D4E495495A96FDFD58CA85B8BBD/scale?width=1200&aspectRatio=1.78&format=jpeg"
+                    alt=""/>
+            </Background>
+            <ImageTitle>
+                <img src="https://lumiere-a.akamaihd.net/v1/images/image_1ebcc00d.png?region=0,0,400,170" alt=""/>
+            </ImageTitle>
+            <Controls>
+                <PlayButton>
+                    <img src="./images/play-icon-black.png" alt=""/>
+                    <span>PLAY</span>
+                </PlayButton>
+                <TrailerButton>
+                    <img src="./images/play-icon-white.png" alt=""/>
+                    <span>Trailer</span>
+
+                </TrailerButton>
+                <AddButton>
+                    <span>+</span>
+                </AddButton>
+                <GroupWatchButton>
+                    <img src="./images/group-icon.png" alt=""/>
+                </GroupWatchButton>
+            </Controls>
+            <SubTitle>
+                2018 7m Family, Fantasy, Kids, Animation
+            </SubTitle>
+            <Description>
+                The Chinese mom from 'Bao' everyone knows
+            </Description>
+        </Container>
+    );
+}
+
+export default Detail;
+
+
+const Container = styled.div`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+`
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  opacity: 0.8;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`
+
+const ImageTitle = styled.div`
+  height: 30vh;
+  width: 35vw;
+  min-height: 170px;
+  min-width: 200px;
+  //margin-top: 60px;
+
+  img {
+    width: 65%;
+    height: 100%;
+    object-fit: contain;
+  }
+
+`
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+
+`
+const PlayButton = styled.button`
+  border-radius: 4px;
+  font-size: 15px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  background-color: rgb(249, 249, 249);
+  border: none;
+  padding: 0 24px;
+  margin-right: 22px;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgb(198, 198, 198);
+  }
+`
+
+const TrailerButton = styled(PlayButton)`
+  border: 1px solid rgb(249, 249, 249);
+  background: rgba(0, 0, 0, 0.3);
+  color: rgb(249, 249, 249);
+  text-transform: uppercase;
+`
+
+const AddButton = styled.button`
+  width: 44px;
+  height: 44px;
+  display: flex;
+  border-radius: 50%;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid white;
+  background-color: rgba(0, 0, 0, 0.6);
+  cursor: pointer;
+  margin-right: 16px;
+
+  span {
+    font-size: 30px;
+    color: white;
+
+  }
+`
+
+const GroupWatchButton = styled(AddButton)`
+  background-color: rgb(0, 0, 0);
+`
+
+const SubTitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+  margin-top: 20px;
+`
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  margin-top: 16px;
+  max-width: 750px;
+`
+
+
+
+
